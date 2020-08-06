8fe7fc91668995aaaba857024b916a2723bd78ff
Veronica L <verolozada12@gmail.com>
2020-08-06 13:36:57 -0400

    Modificaciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,13 @@ $('.botonVer').click(function () {
     $('.ocultar').removeClass('d-none');
     $('.botonVer').addClass('d-none');
     $('.botonMenos').removeClass('d-none');
+    document.getElementById("primertexto").scrollIntoView({behavior: "smooth"});
 });
 
 $('.botonMenos').click(function () {
     $('.ocultar').addClass('d-none');
     $('.botonVer').removeClass('d-none');
     $('.botonMenos').addClass('d-none');
+    document.getElementById("primertexto").scrollIntoView({behavior: "smooth"});
 });
 
